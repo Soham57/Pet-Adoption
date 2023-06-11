b6120262c0996ed107f54929dfb3a2ecc5f6dfe0
soham57 <sohammalviya65@gmail.com>
2023-06-11 19:52:36 +0530

    SearchParams.jsx hooks done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParam";
+import SearchParams from "./SearchParams";
+
 const App = () => {
   return (
     <div>
